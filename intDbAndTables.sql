c9df9e5817fbf521f739b98cdbc65ec06d99b082
roxy22r <raksana280@gmail.com>
2023-05-24 00:37:13 +0200

    init Person data

--- intDbAndTables.sql
+++ intDbAndTables.sql
@@ -7,16 +7,16 @@ GO
 DROP TABLE IF EXISTS Hostpital
 GO
 -- TODO FUNCTION/PROCEDUR to count employees
-CREATE TABLE hostpital(
+CREATE TABLE Hostpital(
 	Id INT IDENTITY(1,1) PRIMARY KEY,
 	NameOfHostpital varchar(255),
-	locationName varchar(255),
+	LocationName varchar(255),
 );
 
 GO
-DROP TABLE IF EXISTS person
+DROP TABLE IF EXISTS Person
 GO
-CREATE TABLE person(
+CREATE TABLE Person(
 	Id INT IDENTITY(1,1) PRIMARY KEY,
 	LastName VARCHAR(255) NOT NULL,
 	FirstName VARCHAR(255) NOT NULL,
@@ -24,48 +24,48 @@ CREATE TABLE person(
 );
 
 GO
-DROP TABLE IF EXISTS disease
-
-CREATE TABLE disease(
-	Id INT IDENTITY(1,1) PRIMARY KEY,
-	Desig VARCHAR(255),
-);
-
-DROP TABLE IF EXISTS patient
+DROP TABLE IF EXISTS Disease
 GO
-CREATE TABLE patient(
+CREATE TABLE Disease(
 	Id INT IDENTITY(1,1) PRIMARY KEY,
-	fk_person int FOREIGN  KEY REFERENCES  person(Id),
-	fk_stationed int FOREIGN  KEY REFERENCES  hostpital(Id),
-	fk_Desig int FOREIGN KEY REFERENCES  disease(Id)
-
+	Desig VARCHAR(255) NOT NULL,
 );
 GO
-DROP TABLE IF EXISTS employees
+DROP TABLE IF EXISTS Patient
 GO
-CREATE TABLE employees(
+CREATE TABLE Patient(
+	Id INT IDENTITY(1,1) PRIMARY KEY,
+	Fk_person int FOREIGN  KEY REFERENCES  Person(Id),
+	Fk_stationed int FOREIGN  KEY REFERENCES  Hostpital(Id),
+	Fk_Desig int FOREIGN KEY REFERENCES  Disease(Id)
+
+);
+GO
+DROP TABLE IF EXISTS Employees
+GO
+CREATE TABLE Employees(
 	Id INT IDENTITY(1,1) PRIMARY KEY,
 	JobTitle VARCHAR(255),
 	HealthSector VARCHAR(255),
 	EntryDate DATETIME,
-	fk_person int FOREIGN KEY REFERENCES  person(Id),
-	fk_WorkingHostpital int FOREIGN KEY REFERENCES  hostpital(Id)
+	Fk_person int FOREIGN KEY REFERENCES  Person(Id),
+	Fk_WorkingHostpital int FOREIGN KEY REFERENCES  Hostpital(Id)
 
 
 
 );
 GO
-DROP TABLE IF EXISTS medicine
-go
+DROP TABLE IF EXISTS Medicine
+Go
 -- Desig stands for Designation
-CREATE TABLE medicine(
+CREATE TABLE Medicine(
 	Id INT IDENTITY(1,1) PRIMARY KEY,
 	Desig VARCHAR(255),
-	Price DOUBLE (9,2),
+	Price DECIMAL (9,2),
 	Producer VARCHAR(255),
 
 );
 
-
+GO
 --Help table
 --CREATE TABLE diseaseHistory();
