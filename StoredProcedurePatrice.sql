e4f98abf363802e7c7f8b0a00b89f9e97821c2fb
roxy22r <raksana280@gmail.com>
2023-06-04 20:26:31 +0200

    Added ToDo's foreach Worker in the project

---
+++ StoredProcedurePatrice.sql
@@ -0,0 +1,12 @@
+-- TODO CREATE A Function Procedures 
+-- To Count Total Patient taking the same medication
+--	 o ToDo create 5 records for patients with the same
+--		medication
+--	 o ToDo mind 3 Test +/-
+
+
+--TODO CREATE Stored Procedures that Creates  a 
+-- new record DiseaseHistory for a Patient.
+-- When any of the values are not it the DB
+-- return a Error MSG
+--	 o ToDo mind 3 Test +/-
