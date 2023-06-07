17b2f0afe672efd340dcc474888e4119b910aab9
patricehotz <polarfuchs750@gmail.com>
2023-06-07 22:52:57 +0200

    Cleanup and refactoring

--- AdditionalProcetureAndFunction.sql
+++ AdditionalProcetureAndFunction.sql
@@ -1,76 +1,79 @@
-DROP FUNCTION IF EXISTS createSimpleSupplierDeliversTable;
+DROP FUNCTION IF EXISTS GetSupplierDeliversTable;
 GO
-CREATE FUNCTION createSimpleSupplierDeliversTable()
-RETURNS TABLE
+
+CREATE FUNCTION GetSupplierDeliversTable()
+	RETURNS TABLE
 AS RETURN
-(SELECT sd.Id,s.CompanyName,h.NameOfHostpital,m.Desig,sd.UnitsInStock,sd.UnitsOnOrder FROM SupplierDelivers sd
-	LEFT JOIN Supplier s ON s.Id = sd.fk_Supplier
-	LEFT JOIN Medicine m ON m.Id = sd.fk_Medication
-	LEFT JOIN Hostpital h ON h.Id = sd.fk_Hospital
-	WHERE sd.fk_Supplier=s.Id
-
-);
-
-GO
-SELECT * FROM createSimpleSupplierDeliversTable();
-GO
-DROP FUNCTION IF EXISTS createSimplePatientTable;
+	(SELECT sd.Id,s.CompanyName,h.NameOfHostpital,m.Desig,sd.UnitsInStock,sd.UnitsOnOrder 
+		FROM SupplierDelivers sd
+		LEFT JOIN Supplier s ON s.Id = sd.fk_Supplier
+		LEFT JOIN Medicine m ON m.Id = sd.fk_Medication
+		LEFT JOIN Hostpital h ON h.Id = sd.fk_Hospital
+		WHERE sd.fk_Supplier=s.Id);
 
 GO
-CREATE FUNCTION createSimplePatientTable()
-RETURNS TABLE 
+
+SELECT * FROM GetSupplierDeliversTable();
+GO
+
+DROP FUNCTION IF EXISTS GetPatientTable;
+GO
+
+CREATE FUNCTION GetPatientTable()
+	RETURNS TABLE
 AS RETURN
-
-(SELECT pati.Id,pers.FirstName,pers.LastName,pers.Birthday,hos.NameOfHostpital,dis.Desig as 'Reason for residence',dis.isHealable  FROM Patient pati
-LEFT JOIN Person pers On pers.Id = pati.Fk_Person
-LEFT JOIN Hostpital hos ON hos.Id = pati.Fk_Stationed
-LEFT JOIN Disease  dis ON dis.Id =pati.Fk_Disease
-WHERE pati.Fk_Person=pers.Id );
-
-GO
-SELECT * FROM createSimplePatientTable();
+	(SELECT pati.Id,pers.FirstName,pers.LastName,pers.Birthday,hos.NameOfHostpital,dis.Desig as 'Reason for residence',dis.isHealable  
+		FROM Patient pati
+		LEFT JOIN Person pers On pers.Id = pati.Fk_Person
+		LEFT JOIN Hostpital hos ON hos.Id = pati.Fk_Stationed
+		LEFT JOIN Disease  dis ON dis.Id =pati.Fk_Disease
+		WHERE pati.Fk_Person=pers.Id);
 GO
 
-
-DROP FUNCTION IF EXISTS createEmployeeSimpleTable;
+SELECT * FROM GetPatientTable();
 GO
-CREATE FUNCTION createEmployeeSimpleTable()
-RETURNS TABLE 
+
+DROP FUNCTION IF EXISTS GetEmployeeTable;
+GO
+
+CREATE FUNCTION GetEmployeeTable()
+	RETURNS TABLE 
 AS RETURN
-(SELECT emp.Id,pers.FirstName,pers.LastName,pers.Birthday,(hos.NameOfHostpital+' '+pla.City) as 'Works at',emp.EntryDate,emp.HealthSector,emp.JobTitle FROM Employee emp
-LEFT JOIN Person pers ON pers.Id = emp.Fk_Person
-LEFT JOIN Hostpital hos ON hos.Id=emp.Fk_WorkingHostpital
-LEFT JOIN Place pla ON pla.Id=hos.Fk_Place);
-
-GO
-SELECT * FROM createEmployeeSimpleTable();
+	(SELECT emp.Id,pers.FirstName,pers.LastName,pers.Birthday,(hos.NameOfHostpital+' '+pla.City) as 'Works at',emp.EntryDate,emp.HealthSector,emp.JobTitle 
+		FROM Employee emp
+		LEFT JOIN Person pers ON pers.Id = emp.Fk_Person
+		LEFT JOIN Hostpital hos ON hos.Id=emp.Fk_WorkingHostpital
+		LEFT JOIN Place pla ON pla.Id=hos.Fk_Place);
 GO
 
-
-
-DROP FUNCTION IF EXISTS createHostpitalSimpleTable;
+SELECT * FROM GetEmployeeTable();
 GO
-CREATE FUNCTION createHostpitalSimpleTable()
-RETURNS TABLE 
+
+DROP FUNCTION IF EXISTS GetHostpitalTable;
+GO
+
+CREATE FUNCTION GetHostpitalTable()
+	RETURNS TABLE 
 AS RETURN
-(SELECT hos.Id,hos.NameOfHostpital,pla.City,pla.PostalCode FROM Hostpital hos
-LEFT JOIN Place pla ON pla.Id = hos.Id);
-
-GO
-SELECT * FROM createHostpitalSimpleTable();
+	(SELECT hos.Id,hos.NameOfHostpital,pla.City,pla.PostalCode 
+		FROM Hostpital hos
+		LEFT JOIN Place pla ON pla.Id = hos.Id);
 GO
 
-
-DROP FUNCTION IF EXISTS createSupplierSimpleTable;
+SELECT * FROM GetHostpitalTable();
 GO
-CREATE FUNCTION createSupplierSimpleTable()
-RETURNS TABLE 
+
+DROP FUNCTION IF EXISTS GetSupplierTable;
+GO
+
+CREATE FUNCTION GetSupplierTable()
+	RETURNS TABLE 
 AS RETURN
-(SELECT sup.Id,sup.CompanyName,pers.FirstName+' '+pers.LastName as 'Owner'  FROM Supplier sup
-LEFT JOIN Person pers ON pers.Id = sup.Id
-);
-
+	(SELECT sup.Id,sup.CompanyName,pers.FirstName+' '+pers.LastName as 'Owner'  
+		FROM Supplier sup
+		LEFT JOIN Person pers ON pers.Id = sup.Id);
 GO
-SELECT * FROM createSupplierSimpleTable();
+
+SELECT * FROM GetSupplierTable();
 GO
 
